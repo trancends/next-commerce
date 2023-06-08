7b38fdb5e143dc2affb2bb7175e3bdc123351a1c
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-06-08 15:51:11 +0700

    create toggle cart icon interactivity, create state for adding product to cart by creating AddCart, and create state hydration,

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -5,7 +5,7 @@ import { useCartStore } from "@/store";
 
 export default function Cart() {
   const cartStore = useCartStore();
-  console.log(cartStore.isOpen);
+
   return (
     <div>
       <h1>Cart</h1>
