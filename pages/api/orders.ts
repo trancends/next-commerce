c48a5c382c8be61de24826aee6afa5bf434bb8bc
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-06-16 16:05:03 +0700

    create orders api to fetch customer's order,fill dashboard page with order data

---
+++ pages/api/orders.ts
@@ -0,0 +1,38 @@
+import Stripe from "stripe";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      // Get the user
+      const user = await getServerSession(req, res, authOptions);
+      if (!user) {
+        res.status(403).json({ message: "Not logged in" });
+      }
+
+      // Find all orders for this user
+      const orders = await prisma.order.findMany({
+        where: {
+          userId: user?.user?.id,
+        },
+        include: {
+          products: true,
+        },
+      });
+      res.status(200).json(orders);
+    } catch (err) {
+      res.status(500).json({ message: "Failed to fetch orders" });
+    }
+  } else {
+    res.setHeader("Allow", "GET");
+    res.status(405).end("Method not allowed");
+  }
+}
